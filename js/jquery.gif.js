0468a8ea94a234982d7c5006042cef0b984ea97f
bing yang <bingyang9258@gmail.com>
2020-09-12 19:56:38 -0400

    add(js): added a js to control gif img; add(index.html): added a emoji design section;

---
+++ js/jquery.gif.js
@@ -0,0 +1,10 @@
+var imageSwap = function () {
+	var $this = $(this);
+	var newSource = $this.data('swap');
+	$this.data('swap', $this.attr('src'));
+	$this.attr('src', newSource);
+}
+
+$(function () {
+	$('img.playgif').hover(imageSwap, imageSwap);
+});
