76b0e4257e99a14396f0e1923650989a7a772ab8
bing yang <bingyang9258@gmail.com>
2020-11-10 22:55:45 -0500

    modified projects

---
+++ js/react_example.js
@@ -0,0 +1,13 @@
+const e = React.createElement;
+
+class Education extends React.Component{
+    render () {
+        return (
+        <h1>ENd</h1>
+        )
+    }
+}
+
+
+const domContainer = document.querySelector('#react_example');
+ReactDOM.render(<Education/>, domContainer);
